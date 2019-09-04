2a2502bb40b193569201cc427b374b485fb7d8e1
Abhi <you@example.com>
2019-09-04 19:53:46 +0530

    new file:   fork.c 	new file:   fork_PID.c 	fork_getpid.c 	fork_more.c 	fork_wait.c 	gate_fork.c

---
+++ fork_PID.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main()
+{
+	int pid;
+	id=fork();
+	if(id==0)
+	{
+	printf("I am child process, PID=\n %d", pid);
+	printf("\n");
+	}
+	else if (pid>0)
+	{
+		printf("I am parent process, PID=\n %d",pid);
+		printf("\n");
+	}
+	else
+	{
+//execlp("le", " ", NULL);
+	printf("error");
+		exit(0);
+	}
+}
